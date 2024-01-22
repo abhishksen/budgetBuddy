37d9e9a1e5079d6ac6d22a0654e7e49fbd126468
Abhishek <codmonk21@gmail.com>
2024-01-22 17:53:25 +0530

    analytics screen UI with charts integration

--- src/screens/analytics/AnalyticsScreen.jsx
+++ src/screens/analytics/AnalyticsScreen.jsx
@@ -29,7 +29,7 @@
 
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import { PieChart } from 'react-native-chart-kit';
+import { BarChart, LineChart, PieChart } from 'react-native-chart-kit';
 import { ScrollView } from 'react-native-gesture-handler';
 
 const AnalyticsScreen = () => {
@@ -72,9 +72,57 @@ const AnalyticsScreen = () => {
         },
     ];
 
+    const totalAmount = data.reduce((total, item) => total + item.amount, 0);
+
     return (
-        <ScrollView style={styles.container}>
+        <ScrollView style={styles.container} showsVerticalScrollIndicator={false}>
+
+            {/* Stats Table and Total Amount */}
+            <View style={styles.statsContainer}>
+                <Text style={styles.statsTitle}>Expense Statistics</Text>
+                <View style={styles.statsTable}>
+                    {data.map((item, index) => (
+                        <View key={index} style={styles.statsRow}>
+                            <Text style={styles.statsCategory}>{item.name}</Text>
+                            <Text style={styles.statsAmount}>{`₹${item.amount.toFixed(2)}`}</Text>
+                        </View>
+                    ))}
+                    <View style={styles.statsRow}>
+                        <Text style={styles.statsCategory}>Total</Text>
+                        <Text style={styles.statsAmount}>{`₹${totalAmount.toFixed(2)}`}</Text>
+                    </View>
+                </View>
+            </View>
+
+            {/* chart container with chart items */}
             <View style={styles.chartContainer}>
+                <Text style={styles.title}>Visualize Your Expense Distribution</Text>
+                {/* Bar Chart */}
+                <BarChart
+                    data={{
+                        labels: data.map(item => item.name),
+                        datasets: [
+                            {
+                                data: data.map(item => item.amount),
+                            },
+                        ],
+                    }}
+                    width={300}
+                    height={200}
+                    // yAxisSuffix="₹"
+                    yAxisLabel="₹"
+                    chartConfig={{
+                        backgroundGradientFrom: '#fff',
+                        backgroundGradientTo: '#fff',
+                        color: (opacity = 1) => `rgba(0, 122, 255, ${opacity})`,
+                    }}
+                    style={{
+                        marginVertical: 8,
+                        borderRadius: 16,
+                    }}
+                />
+
+                {/* pie chart */}
                 <PieChart
                     data={data}
                     width={300}
@@ -82,14 +130,42 @@ const AnalyticsScreen = () => {
                     chartConfig={{
                         backgroundGradientFrom: '#fff',
                         backgroundGradientTo: '#fff',
-                        color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
+                        color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`, // blue color
                     }}
                     accessor="amount"
                     backgroundColor="transparent"
                     paddingLeft="15"
+                    style={{
+                        marginVertical: 10,
+                        borderRadius: 16,
+                    }}
+                />
+
+                {/* Line Chart */}
+                <LineChart
+                    data={{
+                        labels: data.map(item => item.name),
+                        datasets: [
+                            {
+                                data: data.map(item => item.amount),
+                            },
+                        ],
+                    }}
+                    width={300}
+                    height={200}
+                    // yAxisSuffix="₹"
+                    yAxisLabel='₹'
+                    chartConfig={{
+                        backgroundGradientFrom: '#fff',
+                        backgroundGradientTo: '#fff',
+                        color: (opacity = 1) => `rgba(255, 185, 0, ${opacity})`, // Yellow color
+                    }}
+                    style={{
+                        marginVertical: 8,
+                        borderRadius: 16,
+                    }}
                 />
             </View>
-            <Text style={styles.title}>Expense Distribution</Text>
         </ScrollView>
     );
 };
@@ -100,16 +176,48 @@ const styles = StyleSheet.create({
         padding: 16,
         backgroundColor: '#fff',
     },
-    title: {
-        fontSize: 24,
+    statsContainer: {
+        // marginTop: 20,
+        // marginBottom: 20,
+        marginVertical: 10,
+        padding: 16,
+        borderRadius: 10,
+        backgroundColor: '#2C3E50', // Light grey background
+    },
+    statsTitle: {
+        fontSize: 20,
         fontWeight: 'bold',
-        marginTop: 16,
+        marginBottom: 10,
+        color: '#fff',
+    },
+    statsTable: {
+        flexDirection: 'column',
+    },
+    statsRow: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginBottom: 8,
+    },
+    statsCategory: {
+        fontSize: 16,
+        fontWeight: "bold",
+        color: '#fff',
+    },
+    statsAmount: {
+        fontSize: 16,
+        color: '#3498db',
+    },
+    title: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        paddingVertical: 10,
         textAlign: 'center',
         color: '#2C3E50',
     },
     chartContainer: {
         alignItems: 'center',
-        marginTop: 20,
+        marginBottom: 20,
+        paddingBottom: 10,
     },
 });
 
